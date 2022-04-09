ebef6ea937e33e67b3ff6f1013826953b606c957
Ur Sok <mar.dabija@gmail.com>
2022-04-09 23:52:55 +0300

    Added auth to swagger

--- src/WebApi/Program.cs
+++ src/WebApi/Program.cs
@@ -29,6 +29,26 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(opt =>
 {
     opt.SwaggerDoc("v1", new OpenApiInfo { Title = "CodingBattlesApi", Version = "v1" });
+    opt.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+     {
+         Name = "Authorization",
+         Type = SecuritySchemeType.ApiKey,
+         Scheme = "Bearer",
+         BearerFormat = "JWT",
+         In = ParameterLocation.Header,
+         Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 1safsfsdfdfd\"",
+     });
+    opt.AddSecurityRequirement(new OpenApiSecurityRequirement {
+        {
+            new OpenApiSecurityScheme {
+                Reference = new OpenApiReference {
+                    Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    });
 });
 
 #region Add Jwt Auth
